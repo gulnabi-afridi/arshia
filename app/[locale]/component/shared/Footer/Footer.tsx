7b5afdb9f9e3d19b7a3a8d39c8be05f78741503e
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-01-23 01:52:10 +0500

    footer developed

--- app/[locale]/component/shared/Footer/Footer.tsx
+++ app/[locale]/component/shared/Footer/Footer.tsx
@@ -1,9 +1,68 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import * as Icons from '../../../../../svg/Icons';
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+  const { t } = useTranslation();
+
+  //   footer data --->
+  const footerData = {
+    links: [
+      {
+        path: '#',
+        text: t('common:f_home'),
+      },
+      {
+        path: '#',
+        text: t('common:f_about'),
+      },
+      {
+        path: '#',
+        text: t('common:f_documentation'),
+      },
+      {
+        path: '#',
+        text: t('common:f_news_event'),
+      },
+      {
+        path: '#',
+        text: t('common:f_blog'),
+      },
+      {
+        path: '#',
+        text: t('common:f_help'),
+      },
+      {
+        path: '#',
+        text: t('common:f_about'),
+      },
+    ],
+    social: [
+      {
+        path: '#',
+        icon: (
+          <Icons.facebook className='w-[22px] sm:w-[25px] h-[22px] sm:h-[25px]' />
+        ),
+      },
+      {
+        path: '#',
+        icon: (
+          <Icons.instagram className='w-[20px] sm:w-[24px] h-[20px] sm:h-[25px]' />
+        ),
+      },
+      {
+        path: '#',
+        icon: (
+          <Icons.linkedin className='w-[20px] sm:w-[24px] h-[20px] sm:h-[24px]' />
+        ),
+      },
+    ],
+  };
+
   return (
     <div className='w-full'>
       <div className='w-full m-auto max-w-[1400px] gap-4 sm:gap-6 px-4 md:px-8 flex flex-col justify-center items-center'>
@@ -18,7 +77,7 @@ const Footer = () => {
               <Link
                 href={item.path}
                 key={index}
-                className='text-[16px] sm:text-[18px] font-normal text-black-2 relative after:absolute after:w-0 after:bottom-0 after:left-0 after:h-[2px] after:bg-purple-1 hover:after:w-full after:duration-200'
+                className='text-[16px] capitalize sm:text-[18px] font-normal text-black-2 relative after:absolute after:w-0 after:bottom-0 after:left-0 after:h-[2px] after:bg-purple-1 hover:after:w-full after:duration-200'
               >
                 {item.text}
               </Link>
@@ -45,52 +104,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-const footerData = {
-  links: [
-    {
-      path: '#',
-      text: 'Home',
-    },
-    {
-      path: '#',
-      text: 'About itsolve',
-    },
-    {
-      path: '#',
-      text: 'News and events',
-    },
-    {
-      path: '#',
-      text: 'Blog',
-    },
-    {
-      path: '#',
-      text: 'Help',
-    },
-    {
-      path: '#',
-      text: 'Rule',
-    },
-  ],
-  social: [
-    {
-      path: '#',
-      icon: (
-        <Icons.facebook className='w-[22px] sm:w-[25px] h-[22px] sm:h-[25px]' />
-      ),
-    },
-    {
-      path: '#',
-      icon: (
-        <Icons.instagram className='w-[20px] sm:w-[24px] h-[20px] sm:h-[25px]' />
-      ),
-    },
-    {
-      path: '#',
-      icon: (
-        <Icons.linkedin className='w-[20px] sm:w-[24px] h-[20px] sm:h-[24px]' />
-      ),
-    },
-  ],
-};
