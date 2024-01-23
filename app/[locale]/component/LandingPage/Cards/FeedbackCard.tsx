36ebdf65316875920b7c9afcde74ab6e966a8444
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-01-23 13:33:00 +0500

    freelance feedback section done

--- app/[locale]/component/LandingPage/Cards/FeedbackCard.tsx
+++ app/[locale]/component/LandingPage/Cards/FeedbackCard.tsx
@@ -4,26 +4,30 @@ import Image from 'next/image';
 
 const FeedbackCard = () => {
   return (
-    <div className='w-full grid gap-3 grid-cols-[80px,1fr] bg-white-1 rounded-[10px] feedbackSha p-4'>
+    <div className='w-full grid gap-2 sm:gap-3 grid-cols-[70px,1fr] sm:grid-cols-[80px,1fr] justify-start bg-white-1 rounded-[10px] feedbackSha p-3 sm:p-4'>
       {/* left portion --->  */}
       <div className='w-full h-[80px] relative'>
         <Image src='/assets/feedack.svg' fill className='object-fill' alt='' />
       </div>
       {/* right por ---->  */}
-      <div className='flex flex-col w-full gap-3'>
+      <div className='flex flex-col w-full gap-2 sm:gap-3'>
         <div className='w-full flex justify-between items-start'>
           <div className='flex flex-col gap-1'>
-            <p className='text-[18px] font-semibold text-purple-1'>Arshia</p>
+            <p className='text-[16px] sm:text-[18px] font-semibold text-purple-1'>
+              Arshia
+            </p>
             <div className='flex justify-center items-center gap-1'>
               {[0, 1, 2, 3, 4].map((item, index) => {
                 return <Icons.star className='w-[12px] h-[12px]' />;
               })}
             </div>
           </div>
-          <p className='text-[16px] text-black-3 font-normal'>2 days ago</p>
+          <p className='text-[14px] sm:text-[16px] text-black-3 font-normal'>
+            2 days ago
+          </p>
         </div>
         {/* context -->  */}
-        <p className='text-[16px] font-medium text-black-3'>
+        <p className='text-[14px] sm:text-[16px] font-medium text-black-3'>
           The product convinced me a lot, in addition to the attention given and
           the installation, thank you very much. I hope you continue to grow.
           Thank you very much.
--- app/[locale]/component/LandingPage/Feedback.tsx
+++ app/[locale]/component/LandingPage/Feedback.tsx
@@ -6,25 +6,26 @@ import FeedbackCard from './Cards/FeedbackCard';
 
 const Feedback = () => {
   return (
-    <div className='w-full bg-[#f6f6ff] py-16'>
+    <div className='w-full bg-[#f6f6ff] py-10 csm:py-16'>
       <Wrapper>
-        <div className='w-full flex flex-col gap-10'>
-          {/* text ---->  */}
-          <div className='flex flex-col gap-0'>
-            <p className='text-purple-1 text-[22px] font-semibold'>
-              Freelancers feedback
-            </p>
-            <h2 className='text-[32px] text-black-1 font-extrabold'>
-              See what freelancers say about itsolve reviews!
-            </h2>
-          </div>
-          {/* feedback card ----->  */}
-          <div className='w-full gap-4 grid grid-cols-2 '>
-            <FeedbackCard />
-            <FeedbackCard />
-
-            <FeedbackCard />
-            <FeedbackCard />
+        <div className='w-full flex justify-center items-center'>
+          <div className='w-full lg:max-w-full max-w-[600px] flex flex-col gap-4 csm:gap-6 lg:gap-10'>
+            {/* text ---->  */}
+            <div className='flex flex-col gap-0'>
+              <p className='text-purple-1 text-[18px] sm:text-[22px] font-semibold'>
+                Freelancers feedback
+              </p>
+              <h2 className='text-[26px] sm:text-[32px] text-black-1 leading-[38px] sm:leading-[40px] lg:leading-[50px] font-extrabold'>
+                See what freelancers say about itsolve reviews!
+              </h2>
+            </div>
+            {/* feedback card ----->  */}
+            <div className='w-full gap-4 grid grid-cols-1 lg:grid-cols-2 '>
+              <FeedbackCard />
+              <FeedbackCard />
+              <FeedbackCard />
+              <FeedbackCard />
+            </div>
           </div>
         </div>
       </Wrapper>
