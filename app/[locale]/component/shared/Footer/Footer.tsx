1e41b1fbc50d76e3864513dca5d4020b20bc6f49
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-01-25 04:41:43 +0500

    revision one done

--- app/[locale]/component/shared/Footer/Footer.tsx
+++ app/[locale]/component/shared/Footer/Footer.tsx
@@ -61,10 +61,28 @@ const Footer = () => {
         ),
       },
     ],
+    downLinks: [
+      {
+        path: '#',
+        name: t('common:privacy_policy'),
+      },
+      {
+        path: '#',
+        name: t('common:cookie_policy'),
+      },
+      {
+        path: '#',
+        name: t('common:terms_of_use'),
+      },
+      {
+        path: '#',
+        name: t('common:t_statement'),
+      },
+    ],
   };
 
   return (
-    <div className='w-full py-10'>
+    <div className='w-full pt-10'>
       <div className='w-full m-auto max-w-[1400px] gap-4 sm:gap-6 px-4 md:px-8 flex flex-col justify-center items-center'>
         {/* logo --->  */}
         <div className='w-[110px] md:w-[170px] h-[50px] md:h-[70px] relative'>
@@ -97,6 +115,25 @@ const Footer = () => {
               </Link>
             );
           })}
+        </div>
+        {/* most down section ---->  */}
+        <div className='w-full py-6 border-t-[1px] border-black-3/10 gap-y-4 gap-x-10 mt-3 flex flex-col sm:flex-row justify-between items-center '>
+          <p className='text-[16px] text-black-2 font-medium'>
+            {t('common:copy_right')}
+          </p>
+          <div className='flex flex-wrap justify-center sm:justify-start md:justify-center items-center gap-y-2 md:gap-y-4 gap-x-10 xl:gap-x-20'>
+            {footerData.downLinks.map((item, index) => {
+              return (
+                <Link
+                  key={index}
+                  className='text-[16px] text-black-2 font-medium relative after:absolute after:w-0 after:bottom-0 after:left-0 after:h-[2px] after:bg-purple-1 hover:after:w-full after:duration-200'
+                  href={item.path}
+                >
+                  {item.name}
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
--- app/[locale]/component/shared/Navibar/Navibar.tsx
+++ app/[locale]/component/shared/Navibar/Navibar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -11,19 +11,24 @@ import Drawer from 'react-modern-drawer';
 import 'react-modern-drawer/dist/index.css';
 import { RxCross1 } from 'react-icons/rx';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { FaEarthAmericas } from 'react-icons/fa6';
+import OutSideClick from '@/app/hooks/OutSideClick';
 
 const Navibar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { t } = useTranslation();
+  const [openMenu, setOpenMenu] = useState(false);
+  const [smallScreenMenu, setSmallScreenMenu] = useState(false);
 
+  const { t, i18n } = useTranslation();
+  const currentLanguage = i18n.language;
   const toggleDrawer = () => {
     setIsOpen((prevState) => !prevState);
   };
 
   return (
     <React.Fragment>
-      <div className='w-full z-30 bg-white-1 headerSha'>
-        <div className='w-full m-auto max-w-[1400px] h-[60px] md:h-[100px] px-4 md:px-8 gap-4 flex justify-between items-center'>
+      <div className='w-full bg-white-1 headerSha'>
+        <div className='w-full z-0 m-auto max-w-[1400px] py-1 h-[54px] md:h-[94px] px-4 md:px-8 gap-4 flex justify-between items-center'>
           {/* logo -->  */}
           <div className='w-[110px] md:w-[170px] h-[50px] md:h-[70px] relative'>
             <Image src='/assets/logo.svg' alt='' fill className='object-fill' />
@@ -50,6 +55,42 @@ const Navibar = () => {
               {' '}
               {t('common:browse_users')}
             </Link>
+            {/* change language ------->  */}
+            <OutSideClick
+              Event={() => {
+                setOpenMenu(false);
+              }}
+              style='flex flex-col relative z-40'
+            >
+              <button
+                onClick={() => setOpenMenu(!openMenu)}
+                className='hover:opacity-80'
+              >
+                <FaEarthAmericas className='text-[30px] text-purple-1 ' />
+              </button>
+              {openMenu && (
+                <div className='min-w-[160px] bg-white-1 flex flex-col px-2 py-2 rounded-lg shadow-xl absolute top-11 -left-[55px]'>
+                  {languagesSupport.map((item, index) => {
+                    return (
+                      <button
+                        key={index}
+                        onClick={() => {
+                          i18n.changeLanguage(item.key);
+                          setOpenMenu(false);
+                        }}
+                        className={`h-[38px] ${
+                          currentLanguage === item.key
+                            ? 'text-purple-1'
+                            : 'text-black-1'
+                        } hover:text-purple-1 capitalize text-[16px] font-medium`}
+                      >
+                        {item.name}
+                      </button>
+                    );
+                  })}
+                </div>
+              )}
+            </OutSideClick>
             <div className='flex justify-center items-center gap-3'>
               <FillButton
                 styles='bg-purple-1/20 whitespace-nowrap text-purple-1'
@@ -75,7 +116,7 @@ const Navibar = () => {
         direction='right'
         className='drawer'
       >
-        <div className='flex flex-col p-6 justify-center items-center '>
+        <div className='flex flex-col p-6 justify-center items-center'>
           {/* =========>top bar */}
           <div className='w-full flex justify-end items-center'>
             <RxCross1
@@ -90,9 +131,45 @@ const Navibar = () => {
           {/* links  */}
           <div className='flex w-full justify-center items-center flex-col gap-6 mt-6'>
             {/* search */}
-            <div className='w-full h-[50px] pb-0 text-center hideScroll flex items-center px-2 max-w-[350px] whitespace-nowrap overflow-auto text-[14px] text-black-3 font-medium border-[1px] border-white-2 rounded-lg'>
+            <div className='w-full h-[50px] pb-0 text-center hideScroll flex items-center px-2 max-w-[380px] whitespace-nowrap overflow-auto text-[12px] sm:text-[14px] text-black-3 font-medium border-[1px] border-white-2 rounded-lg'>
               {t('common:how_search')}
             </div>
+            {/* change language ------->  */}
+            <OutSideClick
+              Event={() => {
+                setSmallScreenMenu(false);
+              }}
+              style='flex flex-col relative z-40'
+            >
+              <button
+                onClick={() => setSmallScreenMenu(!smallScreenMenu)}
+                className='hover:opacity-80'
+              >
+                <FaEarthAmericas className='text-[30px] text-purple-1 ' />
+              </button>
+              {smallScreenMenu && (
+                <div className='min-w-[160px] bg-white-1 flex flex-col px-2 py-2 rounded-lg shadow-xl absolute top-12 -left-[55px]'>
+                  {languagesSupport.map((item, index) => {
+                    return (
+                      <button
+                        key={index}
+                        onClick={() => {
+                          i18n.changeLanguage(item.key);
+                          setSmallScreenMenu(false);
+                        }}
+                        className={`h-[38px] ${
+                          currentLanguage === item.key
+                            ? 'text-purple-1'
+                            : 'text-black-1'
+                        } hover:text-purple-1 z-50 capitalize text-[16px] font-medium`}
+                      >
+                        {item.name}
+                      </button>
+                    );
+                  })}
+                </div>
+              )}
+            </OutSideClick>
             {/* docs --->  */}
             <Link
               href='#'
@@ -127,5 +204,16 @@ const Navibar = () => {
     </React.Fragment>
   );
 };
+
+const languagesSupport = [
+  {
+    key: 'en',
+    name: 'english',
+  },
+  {
+    key: 'fr',
+    name: 'French',
+  },
+];
 
 export default Navibar;
